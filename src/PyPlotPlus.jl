b250b8689f5a106e4ef69ecf1cf79093fae9ee65
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2018-03-13 15:00:02 -0400

    Small changes

--- src/PyPlotPlus.jl
+++ src/PyPlotPlus.jl
@@ -6,32 +6,29 @@ module PyPlotPlus
 
 using PyCall, NullableArrays
 
+export makesquare!, ticksoff!, removespines!, getbasicoutput, axisright!, PyObject
+
 @pyimport numpy.ma as ma                                                                                                
 PyObject(a::NullableArray) = pycall(ma.array, Any, a.values, mask=a.isnull) 
-
-export makesquare!, ticksoff!, removespines!, getbasicoutput, axisright!                                                
                                                                                                                         
-# Stuff to help with plotting                                                                                           
-makesquare!(ax) = ax[:set_aspect](1, adjustable="box")                                                                  
-makesquare!(axs::AbstractArray) = for ax in axs; makesquare!(ax); end                                                   
+makesquare!(ax) = ax[:set_aspect](1, adjustable="box")
+makesquare!(axs::AbstractArray) = for ax in axs; makesquare!(ax); end
                                                                                                                         
-ticksoff!(a) = a[:tick_params](bottom=false, left=false, labelbottom=false,                                             
-  labelleft=false)                                                                                                      
-ticksoff!(axs::AbstractArray) = for ax in axs; ticksoff!(ax); end                                                       
+ticksoff!(ax) = ax[:tick_params](bottom=false, left=false, labelbottom=false, labelleft=false)
+ticksoff!(axs::AbstractArray) = for ax in axs; ticksoff!(ax); end
                                                                                                                         
-function removespines!(a)                                                                                               
-  for spine in ["top", "bottom", "left", "right"]                                                                       
-    a[:spines][spine][:set_visible](false)                                                                              
-  end                                                                                                                   
-  nothing                                                                                                               
+function removespines!(ax)
+  for spine in ["top", "bottom", "left", "right"]
+    ax[:spines][spine][:set_visible](false)
+  end
+  nothing
+end
+                                                                                                                        
+function axisright!(ax)
+  ax[:tick_params](axis="y", which="both", left=false, labelleft=false, right=true, labelright=true)                                                                                        
+  ax[:yaxis][:set_label_position]("right")
 end                                                                                                                     
                                                                                                                         
-function axisright!(ax)                                                                                                 
-  ax[:tick_params](axis="y", which="both", left=false, labelleft=false,                                                 
-    right=true, labelright=true)                                                                                        
-  ax[:yaxis][:set_label_position]("right")                                                                              
-end                                                                                                                     
-                                                                                                                        
-removespines!(axs::AbstractArray) = for ax in axs; removespines!(ax); end  
+removespines!(axs::AbstractArray) = for ax in axs; removespines!(ax); end
 
 end
