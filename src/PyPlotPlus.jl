19d6c5b9660ff057bfc282dc0e827f70351a43fd
Gregory Wagner <wagner.greg@gmail.com>
2018-06-15 12:58:17 -0400

    Updates

--- src/PyPlotPlus.jl
+++ src/PyPlotPlus.jl
@@ -4,23 +4,51 @@ module PyPlotPlus
 
 using PyPlot, PyCall
 
-export makesquare, ticksoff, axisright, tightshow,
-       removespine, removespines, cornerspines, bottomspine,
-       invertaxis
+export makesquare, ticksoff, tickson, axisright, tightshow, axisright,
+       removespine, removespines, cornerspines, bottomspine, invertaxis, 
+       aspectratio, usecmbright, scalartickformat, tickparams
 
-makesquare(ax) = ax[:set_aspect](1, adjustable="box")
+tickparams(ax=gca(); kwds...) = ax[:tick_params](; kwds...)
+
+function usecmbright()
+  rc("text.latex", preamble="\\usepackage{cmbright}")
+  rc("font", family="sans-serif")
+  nothing
+end
+
+function scalartickformat(ax=gca())
+  ax[:get_xaxis]()[:set_major_formatter](matplotlib[:ticker][:ScalarFormatter]())
+  ax[:get_xaxis]()[:set_minor_formatter](matplotlib[:ticker][:NullFormatter]())
+  nothing
+
+function scalartickformat(ax=gca())
+  ax[:get_xaxis]()[:set_major_formatter](matplotlib[:ticker][:ScalarFormatter]())
+  ax[:get_xaxis]()[:set_minor_formatter](matplotlib[:ticker][:NullFormatter]())
+  nothing
+end
+
+aspectratio(a, ax=gca(); adjustable="box") = ax[:set_aspect](a, adjustable=adjustable)
+makesquare(ax=gca()) = aspectratio(1, ax)
 makesquare(axs::AbstractArray) = for ax in axs; makesquare(ax); end
-                                                                                                                        
-ticksoff(ax) = ax[:tick_params](bottom=false, left=false, labelbottom=false, labelleft=false)
-ticksoff(axs::AbstractArray) = for ax in axs; ticksoff(ax); end
 
-function ticksoff(ax, side)
+function setticks(ax, side, turnon)
+  if side in ["top", "bottom"]
+    axiskw = "x"
+  elseif side in ["left", "right"]
+    axiskw = "y"
+  end
   sidekw = Symbol(side)
   labelsidekw = Symbol(:label, side)
-  keywords = Dict(sidekw=>false, labelsidekw=>false)
+  keywords = Dict(:axis=>axiskw, :which=>"both", sidekw=>turnon, labelsidekw=>turnon)
   ax[:tick_params](keywords)
   nothing
 end
+
+tickson(ax, side) = setticks(ax, side, true)
+
+ticksoff(ax, side) = setticks(ax, side, false)
+ticksoff(ax) = ax[:tick_params](bottom=false, left=false, labelbottom=false, labelleft=false)
+ticksoff(axs::AbstractArray) = for ax in axs; ticksoff(ax); end
 
 "Invert the axis specified by keyword `axis`. Defaults to the yaxis."
 function invertaxis(ax=gca(); axis="y")
@@ -37,9 +65,10 @@ end
 "Remove `spine` from `ax`."
 function removespine(ax, spine) 
   ax[:spines][spine][:set_visible](false)
+  ticksoff(ax, spine)
   nothing
 end
-                                                                                                                        
+
 "Remove all spines."
 function removespines(ax=gca())
   for spine in ["top", "bottom", "left", "right"]
@@ -49,11 +78,18 @@ function removespines(ax=gca())
 end
 
 "Remove all spines except bottom and left spines."
-function cornerspines(ax=gca())
-  for spine in ["top", "right"]
+function cornerspines(ax=gca(); side="left")
+  if !(side == "left" || side == "right")
+    throw("side must be left or right")
+  end
+
+  sidetoremove = side == "left" ? "right" : "left"
+  for spine in ["top", sidetoremove]
     removespine(ax, spine)
   end
-  ax[:tick_params](right=false, labelright=false, top=false, labeltop=false)
+  tickson(ax, "bottom")
+  tickson(ax, side)
+  ax[:yaxis][:set_label_position](side)
   nothing
 end
 
@@ -61,13 +97,13 @@ end
 function bottomspine(ax=gca())
   for spine in ["top", "right", "left"]
     removespine(ax, spine)
+    ticksoff(ax, spine)
   end
-  ax[:tick_params](left=false, labelleft=false, right=false, labelright=false, top=false, labeltop=false)
   nothing
 end
-                                                                                                                        
+
 "Move axis labels and ticks to the right side."
-function axisright(ax)
+function axisright(ax=gca())
   ax[:tick_params](axis="y", which="both", left=false, labelleft=false, right=true, labelright=true)
   ax[:yaxis][:set_label_position]("right")
 end
